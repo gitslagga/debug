ac4300d85832d0930159c3cfbeccc9693fb0007e
Jean <chenshuaijunpp@icloud.com>
2019-01-08 16:59:59 +0800

    add debug

---
+++ src/Utils.php
@@ -0,0 +1,31 @@
+<?php
+namespace Jean\Library;
+
+class Utils 
+{
+    private $name;
+    public function __construct( $name = 'World' )
+    {
+        $this->$name = $name;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function hello()
+    {
+        return 'Hello '.$this->name.'!';
+    }
+
+    public function debug($data = []) {
+        if (is_array($data) || is_object($data)) {
+            echo "<pre>";
+            var_dump($data);
+            echo "</pre>";
+        } else {
+            echo "<pre>{$data}</pre>";
+        }
+    }
+}
