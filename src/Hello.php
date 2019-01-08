131060fc94430126571bd14c805b9b495121f037
Jean <chenshuaijunpp@icloud.com>
2019-01-08 20:59:52 +0800

    fix

--- src/Hello.php
+++ src/Hello.php
@@ -6,7 +6,7 @@ class Hello
     private $name;
     public function __construct($name = 'World')
     {
-        $this->$name = $name;
+        $this->name = $name;
     }
 
     public function getName()
